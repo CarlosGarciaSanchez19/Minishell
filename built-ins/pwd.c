6a49be960afc90f86576971d732bad85d29acef7
Carlos Garcia Sanchez <carlosg2@c2r3s3.42madrid.com>
2025-01-10 16:20:01 +0100

    Modifying pwd function

--- built-ins/pwd.c
+++ built-ins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: carlosg2 <carlosg2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 15:55:37 by carlosg2          #+#    #+#             */
-/*   Updated: 2025/01/10 15:00:48 by carlosg2         ###   ########.fr       */
+/*   Updated: 2025/01/10 16:15:30 by carlosg2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,11 @@
 
 int	ft_pwd(t_shell *shell)
 {
-	char	*pwd;
-
-	pwd = my_getenv("PWD", shell->envp);
-	if (!pwd)
+	if (!shell->pwd)
 	{
 		ft_printf("PWD not set.\n");
 		return (0);
 	}
-	ft_printf("%s\n", pwd);
+	ft_printf("%s\n", shell->pwd);
 	return (1);
 }
